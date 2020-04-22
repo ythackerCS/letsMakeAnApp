6a34d127482ee98f7dccf82c05bce93eca182b12
Alex Appel <alex.appel97@gmail.com>
2020-04-22 06:23:05 -0700

    Update Bookmarked view

--- lettuce/lettuce/FavoritesViewController.swift
+++ lettuce/lettuce/FavoritesViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import FirebaseFirestore
 import FirebaseAuth
+import CoreLocation
 
 class FavoritesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -17,9 +18,22 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     var events:[DocumentSnapshot] = []
     var selectedItemIndex = 0
     
-    func initView(){
+    let locManager = CLLocationManager()
+    
+    var currentLocation: CLLocation!
+    
+    func initView() {
         theTableView.delegate = self
         theTableView.dataSource = self
+        
+        locManager.requestWhenInUseAuthorization()
+
+        if
+           CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+           CLLocationManager.authorizationStatus() ==  .authorizedAlways
+        {
+            currentLocation = locManager.location
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,9 +43,9 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if let myCell = theTableView.dequeueReusableCell(withIdentifier: "eventDetails") as? EventCard2 {
-                        
+            
             if let title = events[indexPath.item].get("name") as? String{
-//                print("getting title")
+                //                print("getting title")
                 myCell.eventTitle.text = title
                 myCell.eventTitle.adjustsFontSizeToFitWidth = true
                 myCell.eventTitle.minimumScaleFactor = 0.5
@@ -40,62 +54,69 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
                 print("Couldn't parse title")
             }
             
-            if let categoryIconURL = events[indexPath.item].get("photos_url") as? String {
-                if let categoryIconActualURL = URL(string: categoryIconURL){
-                    let categoryIconData = try? Data(contentsOf: categoryIconActualURL)
-                    if let data = categoryIconData {
-                        let categoryIcon = UIImage(data: data)
-                        myCell.categoryIcon.image = categoryIcon
-                        myCell.categoryIcon.contentMode = .scaleAspectFill
-//                        myCell.noImgLabel.isHidden = true
-                    }
-                    else {
-                        print("could not get data")
-                        myCell.categoryIcon.image = nil
-    //                        myCell.noImgLabel.isHidden = false
+            myCell.categoryIcon.image = nil
+            if let categoryIconURL = events[indexPath.item].get("photos_url") as? String,
+                let categoryIconActualURL = URL(string: categoryIconURL) {
+                
+                DispatchQueue.global(qos: .background).async {
+                    // fetch image on background thread
+                    if let categoryIconData = try? Data(contentsOf: categoryIconActualURL) {
+                        DispatchQueue.main.async {
+                            // update image on main thread
+                            let categoryIcon = UIImage(data: categoryIconData)
+                            myCell.categoryIcon.image = categoryIcon
+                            myCell.categoryIcon.contentMode = .scaleAspectFill
+                        }
                     }
                 }
-                else {
-                    myCell.categoryIcon.image = nil
-//                    myCell.noImgLabel.isHidden = false
-                    
+            }
+            
+            myCell.locationMarker.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/6)
+            //            myCell.locationMarker.tintColor = UIColor.label
+            
+            let templateImage = myCell.locationMarker?.image?.withRenderingMode(.alwaysTemplate)
+            myCell.locationMarker.image? = templateImage!
+            myCell.locationMarker.tintColor = UIColor.label
+            
+            if let location = events[indexPath.item].get("location") as? GeoPoint {
+                let dist = distance(lat1: self.currentLocation.coordinate.latitude, lon1: self.currentLocation.coordinate.longitude, lat2: location.latitude, lon2: location.longitude)
+                
+                myCell.distance.text = String(format:"%.1f mi.", dist)
+            }
+            
+            if let timeStamp = events[indexPath.item].get("date_time") as? Timestamp {
+                let date = timeStamp.dateValue()
+                
+                let formatter = DateFormatter()
+                formatter.dateFormat = "MM/dd/yyyy"
+                let dateLabel = formatter.string(from: date)
+                
+                myCell.eventDate.text = dateLabel
+            }
+            
+            if let going = events[indexPath.item].get("going") as? [String] {
+                let numGoing = going.count
+                
+                switch (numGoing) {
+                case _ where numGoing <= 5:
+                    myCell.personIcon1.isHidden = true
+                    myCell.personIcon2.isHidden = true
+                    break
+                case _ where (numGoing > 5 && numGoing <= 11):
+                    myCell.personIcon1.isHidden = true
+                    break
+                default:
+                    break
                 }
-            }else{
-//                myCell.noImgLabel.isHidden = false
-                myCell.categoryIcon.image = nil
-    //                print("Couldn't parse url")
             }
-            
-//            if let description = events[indexPath.item].get("description") as? String{
-//                myCell.descriptionLabel.text = description
-//            }else{
-//                print("Couldn't parse description")
-//            }
-            
-//            if let username = events[indexPath.item].get("username") as? String{
-//                myCell.userNameLabel.text = username
-//            }else{
-//                print("Couldn't parse username")
-//            }
-            
-            if let location = events[indexPath.item].get("address") as? String {
-//                myCell.distance.text = location
-                myCell.distance.text = "20 mi."
-            }else{
-//                print("Couldn't parse location")
-//                myCell.userNameLabel.text = nil
-            }
-            
-//            myCell.descriptionLabel.isScrollEnabled = false
-//            myCell.descriptionLabel.isEditable = false
             
             myCell.bookmarkIcon.isSelected = true
             
-//            myCell.favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
-//            myCell.favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+            myCell.bookmarkIcon.setImage(UIImage(systemName: "bookmark"), for: .normal)
+            myCell.bookmarkIcon.setImage(UIImage(systemName: "bookmark.fill"), for: .selected)
             
             myCell.bookmarkIcon.addTarget(self, action: #selector(unfavorite(button:)), for: .touchUpInside)
-
+            
             myCell.bookmarkIcon.tag = indexPath.row
             
             return myCell
@@ -141,7 +162,7 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
                 } else {
                     for document in querySnapshot!.documents {
                         let favoritedEventsList = document.get("favoritedEventsList") as! [String]
-                                                    
+                        
                         for event in favoritedEventsList {
                             
                             let docRef = db.collection("events").document(event)
@@ -155,9 +176,9 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
                                 }
                                 self.theTableView.reloadData()
                             }
-                               
+                            
                         }
-
+                        
                     }
                 }
         }
@@ -170,14 +191,14 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
         
         let buttonTag = button.tag
         
-//        print("fire")
-//        print(buttonTag)
-//
-//        print("highlighted: ")
-//        print(button.isHighlighted)
-//        print(button.isSelected)
-//
-//        print(button)
+        //        print("fire")
+        //        print(buttonTag)
+        //
+        //        print("highlighted: ")
+        //        print(button.isHighlighted)
+        //        print(button.isSelected)
+        //
+        //        print(button)
         
         let documentID = events[buttonTag].documentID
         
@@ -189,17 +210,43 @@ class FavoritesViewController: UIViewController, UITableViewDelegate, UITableVie
                     print("Error getting documents: \(err)")
                 } else {
                     for document in querySnapshot!.documents {
-//                        print("unfavorited event!")
-                       // Atomically remove an event from the "favoritedEventList" array field.
-                       document.reference.updateData([
-                           "favoritedEventsList": FieldValue.arrayRemove([documentID])
-                       ])
+                        //                        print("unfavorited event!")
+                        // Atomically remove an event from the "favoritedEventList" array field.
+                        document.reference.updateData([
+                            "favoritedEventsList": FieldValue.arrayRemove([documentID])
+                        ])
                         self.events.remove(at: buttonTag)
                     }
                     self.theTableView.reloadData()
                 }
         }
-    
-
+        
+        
     }
+    
+    // The following was obtained from:
+    // https://www.geodatasource.com/developers/swift
+    
+    func deg2rad(deg:Double) -> Double {
+        return deg * Double.pi / 180
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///  This function converts radians to decimal degrees              ///
+    ///////////////////////////////////////////////////////////////////////
+    func rad2deg(rad:Double) -> Double {
+        return rad * 180.0 / Double.pi
+    }
+
+    func distance(lat1:Double, lon1:Double, lat2:Double, lon2:Double) -> Double {
+        let theta = lon1 - lon2
+        var dist = sin(deg2rad(deg: lat1)) * sin(deg2rad(deg: lat2)) + cos(deg2rad(deg: lat1)) * cos(deg2rad(deg: lat2)) * cos(deg2rad(deg: theta))
+        dist = acos(dist)
+        dist = rad2deg(rad: dist)
+        dist = dist * 60 * 1.1515
+        // In miles
+        dist = dist * 0.8684
+        return dist
+    }
+
 }
